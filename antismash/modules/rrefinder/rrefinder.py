3906c8c965f9a596ca7b86bd0ae98be843fe80b0
amkloosterman <a.m.kloosterman@biology.leidenuniv.nl>
2020-08-12 11:36:46 +0200

    Wrote to and from json functions for RREFinder results; restructured how the hits from HmmResults are processed

--- antismash/modules/rrefinder/rrefinder.py
+++ antismash/modules/rrefinder/rrefinder.py
@@ -11,75 +11,68 @@ from antismash.common.hmmer import run_hmmer_copy, HmmerResults
 from antismash.common import path
 
 from antismash.common.secmet import Region
+from antismash.common.secmet.features import Domain, Feature, FeatureLocation
 
 class RREFinderResults(ModuleResults):
     """ Results class for the RREFinder analysis"""
     schema_version = 1  # when the data format in the results changes, this needs to be incremented
 
-    # define whatever construction arguments are needed, record_id is required by the superclass
-    # it's good to keep any command line option values here to know when they're changed for --reuse-results
-    def __init__(self, record_id: str, cutoff: float, min_length: int, hits: Dict[int, Dict[str, List[Dict[str, Any]]]]) -> None:
+    def __init__(self, record_id: str, bitscore_cutoff: float, min_length: int, hits_per_protocluster: Dict[int, Dict[str, List[Dict[str, Any]]]]) -> None:
         super().__init__(record_id)
         # The cutoff used for hmmscan
-        self.cutoff = cutoff
+        self.bitscore_cutoff = bitscore_cutoff
         self.min_length = min_length
         # All the hits per locus_tag, per protocluster ID
-        self.hits = hits
-#        # Keep protoclusters with RRE hits
-#        self.clusters = defaultdict(set)  # type: Dict[int, Set[str]]
-#        # e.g. self.clusters[cluster_number] = {gene1_locus, gene2_locus}
+        self.hits_per_protocluster = hits_per_protocluster
+        self.features = [] # type: List[Feature] # features created for RREs
 
-    # implement a conversion to a JSON-compatible dictionary
-    # all elements must one of: str, int, float, list, or a dict of those types (this can recurse)
+    def add_to_record(self, record: Record) -> None:
+        """ Adds the analysis results to the record """
+        if record.id != self.record_id:
+            raise ValueError("Record to store in and record analysed don't match")
+        raise NotImplementedError()  # remove this when completed
+
     def to_json(self) -> Dict[str, Any]:
         """ Constructs a JSON representation of this instance """
 
         return {
             "schema_version": self.schema_version,
-            "cutoff": self.cutoff,
-            "hits": self.hits,
-#            "protoclusters": {key: list(val) for key, val in self.clusters.items()}
+            "bitscore_cutoff": self.bitscore_cutoff,
+            "hits_per_protocluster": self.hits_per_protocluster,
+            "min_length": self.min_length,
+            "record_id": self.record_id,
         }
 
-    # once _all_ analysis modules have completed, their results are added with this method
-    # adding to the record during the analysis will cause issues
-    def add_to_record(self, record: Record) -> None:
-        """ Adds the analysis results to the record """
-        if record.id != self.record_id:
-            raise ValueError("Record to store in and record analysed don't match")
-        # any results would be added here
-        # for an example of new features, see antismash.modules.tta
-        # for an example of qualifiers, see antismash.modules.t2pks
-        # any new feature types or qualifiers would be implemented in antismash.common.secmet,
-        #   and would need to be able to be converted to and from biopython's SeqFeature without loss
-        # raise NotImplementedError()  # remove this when completed
-
-    # implement a conversion from the JSON-compatible data returned by to_json()
-    # this allows --results-reuse to avoid running the module again if not neccessary
     @staticmethod
-    def from_json(json: Dict[str, Any], record: Record) -> Optional["RREFinderResults"]:
-        """ Constructs a new results instance from a JSON format and the
-            original record analysed.
+    def from_json(json: Dict[str, Any], record: Record, min_length: int, bitscore_cutoff: float) -> Optional["RREFinderResults"]: # type: ignore  # pylint: disable=arguments-differ
+        """ Regenerates the RREFinderResults from json.
+            If less strict settings are given (e.g. a lower cutoff
+            or a shorter minimum length), the results are discarded.
         """
+
         # check that the previous data version is the same as current, if not, discard the results
         if json["schema_version"] != RREFinderResults.schema_version:
             return None
 
-        # as an example, checking if the example cutoff option matches that of the previous run
-        options = get_config()
-        if options.template_cutoff != json["cutoff"]:
-            # it's nice to log some decisions to the debug logger so that it's easier to follow
-            logging.debug("RREFinderResults cutoff has changed, discarding previous results")
+        if record.id != json.get("record_id"):
+            logging.warning("RREFinder results are for different record, discarding previous results")
             return None
 
-        # the exact reconstruction depends on what details are stored
-        # to match the conversion to JSON that would be:
-        results = RREFinderResults(json["record_id"], json["cutoff"])
-        for other in json["other"]:
-            results.some_other_information.append(other)
+        prev_min_length = json.get("min_length")
+        prev_bitscore_cutoff = json.get("bitscore_cutoff")
 
-        return results
+        # Check if the cutoff options set in this run are the same or stricter than in the previous run
+        if bitscore_cutoff < prev_bitscore_cutoff:
+            logging.debug("RREFinderResults bitscore cutoff has changed, discarding previous results")
+            return None
+        elif min_length < prev_min_length:
+            logging.debug("RREFinderResults minimum length has changed, discarding previous results")
+            return None
 
+        # Refilter the hits (in case the cutoff is now more stringent)
+        filtered_hits = filter_hits(json['hits_per_protocluster'], min_length, bitscore_cutoff)
+
+        return RREFinderResults(record.id, bitscore_cutoff, min_length, filtered_hits)
 
 def is_ripp(product: str) -> bool:
     ripp_products = ['bacteriocin','cyanobactin','lanthipeptide',
@@ -87,56 +80,46 @@ def is_ripp(product: str) -> bool:
                       'proteusin','glycocin','bottromycin','microcin']
     return product in ripp_products
     
-def hmmscan_rrefinder(record: Record, bitscore_cutoff: float, min_length: int) -> Dict[int, Dict[str, List[Dict[str, Any]]]]:
+def run_hmmscan_rrefinder(record: Record, bitscore_cutoff: float) -> Dict[int, List[Dict[str, Any]]]:
     max_evalue = 1 # Mandatory argument for hmmscan, but hits are only filtered on score here
     hmm_database = path.get_full_path(__file__, 'data', 'RREFinder.hmm')
     
-    # Analyse each CDS in each protocluster
-    # This is redundant in the case of overlapping RiPP protoclusters
-    hmm_hits = defaultdict(lambda: defaultdict(list))
+    # Scan each CDS in each protocluster
+    # This is somewhat redundant in the case of overlapping RiPP protoclusters,
+    # as some locus_tags may be scanned twice
+
+    hmm_hits = defaultdict(lambda: defaultdict(list)) # type: Dict[int, Dict[str, List[Dict[str, Any]]]]
     for region in record.get_regions():
         for protocluster in region.get_unique_protoclusters():
             if is_ripp(protocluster.product):
                 protocluster_number = protocluster.get_protocluster_number()
-                hmm_result = run_hmmer_copy(record, protocluster.cds_children, max_evalue, bitscore_cutoff, min_length, hmm_database, 'rrefinder')
-                hmm_hits_filtered = filter_hmm_hits_min_length(hmm_result.hits, min_length)
-                if hmm_hits_filtered:
-                    for hit in hmm_hits_filtered:
-                        hmm_hits[protocluster_number][hit['locus_tag']].append(hit)
+                hmm_result = run_hmmer_copy(record, protocluster.cds_children, max_evalue, bitscore_cutoff, hmm_database, 'rrefinder')
+                for hit in hmm_result.hits:
+                    hmm_hits[protocluster_number][hit['locus_tag']].append(hit)
     return hmm_hits
-    
-def filter_hmm_hits_min_length(hits: List[Dict[str, Any]], min_length: int) -> List[Dict[str, Any]]:
-    filtered_hits = []
-    for hit in hits:
-        length = hit['protein_end'] - hit['protein_start']
-        if length >= min_length:
-            filtered_hits.append(hit)
+
+def filter_hits(hits: Dict[int, Dict[str, List[Dict[str, Any]]]], min_length: int, bitscore_cutoff: float) -> Dict[int, Dict[str, List[Dict[str, Any]]]]:
+    filtered_hits = defaultdict(lambda: defaultdict(list)) # type: Dict[int, Dict[str, List[Dict[str, Any]]]]
+    for protocluster, locus_tags in hits.items():
+        for locus_tag, hits_locus_tag in locus_tags.items():
+            for hit in hits_locus_tag:
+                if check_hmm_hit(hit, min_length, bitscore_cutoff):
+                    filtered_hits[protocluster][locus_tag].append(hit)
     return filtered_hits
-    
-#def organize_hits_per_locustag(hits_per_protocluster: List[Dict[str, Any]]) -> Dict[str, Dict[str, Any]]):
-#    hits_per_locus_tag = {}
-#    for hit in hits_
 
-#def collect_hmm_hits(hmm_results: List[HmmerResults]) -> List[Dict[str, Any]]:
-#    hits = []
-#    for hmm_result in hmm_results:
-#        hits.extend(hmm_result.hits)
-#    return hits
+def check_hmm_hit(hit: Dict[str, Any], min_length: int, bitscore_cutoff: float) -> Dict[str, Any]:
+    return (hit['protein_end'] - hit['protein_start'] + 1) >= min_length and (hit['score'] >= bitscore_cutoff)
+    # +1 to correct for normal (non-pythonic) indeces, which are used in biopython feature locations
+    # double check this to be sure
 
-def run_rrefinder(record: Record, bitscore_cutoff: float) -> RREFinderResults:
-    
-#    nr_ripps = count_ripps(record)
-#    logging.critical('Found %i RiPP protocluster(s).' %nr_ripps)
-#    results = RREFinderResults(record.id, bitscore_cutoff, nr_ripps)
-    min_length = 50 #TODO: Add this as a commandline argument for antiSMASH
-    # Run hmmscan per protocluster
-    hmm_hits = hmmscan_rrefinder(record,bitscore_cutoff,min_length)
-    # Gather all the hits
-    RRE_results = RREFinderResults(record.id,bitscore_cutoff,min_length,hmm_hits)    
-    # Add the hits for each protocluster
-    logging.critical('Found hits in %i protoclusters.' %len(RRE_results.hits))
-    
+def run_rrefinder(record: Record, bitscore_cutoff: float, min_length: int) -> RREFinderResults:
+    # Run hmmscan per protocluster and gather the hits
+    hmm_hits = hmmscan_rrefinder(record, bitscore_cutoff, min_length)
+    # Filter the hits
+    filtered_hits = filter_hits(hmm_hits, min_length, bitscore_cutoff)
+    # Convert to RREFinderResults object
+    RRE_results = RREFinderResults(record.id, bitscore_cutoff, min_length, filtered_hits)
+    # Temporary
+    logging.critical('Found hits in %i protoclusters.' %len(RRE_results.hits_per_protocluster))
+
     return RRE_results
-    
-    
-    
